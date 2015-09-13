f74965841f0b7c7048a6b8be3e338c64b00821ed
bh_levi <zeppike@gmail.com>
2015-09-13 11:09:56 +0200

    Improved output for more shades of gray

--- AsciiArt.py
+++ AsciiArt.py
@@ -1,4 +1,13 @@
 from PIL import Image
+import os.path
+
+
+class Pixel:
+    def __init__(self, rgb):
+        self.rgb = rgb
+
+    def get_intensity(self):
+        return sum(self.rgb)/765.0
 
 
 class AsciiArt:
@@ -6,20 +15,26 @@ class AsciiArt:
         if img_file_path != "JUST_FOR_TESTING":
             self._load_image(img_file_path)
 
-    FINAL_WIDTH = 100.0
+    FINAL_WIDTH = 100
 
     def _load_image(self, img_file_path):
         self.img = Image.open(img_file_path)
         self.px = self.img.load()
+        self.pixel_size = self._calculate_pixel_size()
 
     def _calculate_pixel_size(self):
         return self.img.width / self.FINAL_WIDTH
 
+    def get_char(self, intensity):
+        intensity_string = " .:-=+*#%@"[::-1]
+        steps = len(intensity_string)
+        return intensity_string[int(steps * intensity - 1)]
+
     def draw(self):
         img_string = ""
-        for y in range(self.img.height)[::2]:
-            for x in range(self.img.width)[::2]:
-                char_to_draw = " " if self.px[x, y][0] < 255 else "#"
+        for y in range(self.img.height)[::self.pixel_size]:
+            for x in range(self.img.width)[::self.pixel_size]:
+                char_to_draw = self.get_char(Pixel(self.px[x, y]).get_intensity())
                 img_string += char_to_draw
             img_string += "\n"
         return img_string
@@ -36,6 +51,10 @@ if __name__ == '__main__':
     if args.file is None:
         print("Add image path to work with!")
         exit()
+    elif not os.path.isfile(args.file):
+        print("No Image "+args.file+" found!")
+        exit()
+
 
     a = AsciiArt(args.file)
     print(a.draw())
--- test_AsciiArt.py
+++ test_AsciiArt.py
@@ -1,5 +1,5 @@
 import unittest
-from AsciiArt import AsciiArt
+from AsciiArt import AsciiArt, Pixel, PixelArray
 
 
 class AsciiArtTests(unittest.TestCase):
@@ -10,9 +10,13 @@ class AsciiArtTests(unittest.TestCase):
     def test_calculate_pixel_size(self):
         AsciiObj = AsciiArt("epam.png")
         ps = AsciiObj._calculate_pixel_size()
-        self.assertAlmostEqual(ps, 288.0 / 100, 2)
+        self.assertAlmostEqual(ps, 288 / 100, 2)
 
     def test_draw_method(self):
         AsciiObj = AsciiArt("epam.png")
         img = AsciiObj.draw()
         self.assertGreater(len(img), 0)
+
+    def test_pixel_object(self):
+        px = Pixel((255,255,255))
+        self.assertEqual(px.get_intensity(), 255*3)
